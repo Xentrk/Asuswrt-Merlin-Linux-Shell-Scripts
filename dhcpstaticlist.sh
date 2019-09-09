3db01c18993bc54c809982e79946e75959429aeb
Xentrk <xentrk@x3mtek.com>
2019-09-09 20:54:18 +0700

    update for 384.13 release

--- dhcpstaticlist.sh
+++ dhcpstaticlist.sh
@@ -2,8 +2,8 @@
 ####################################################################################################
 # Script: dhcpstaticlist.sh
 # Original Author: Xentrk
-# Last Updated Date: 30-June-2019
-# Version 1.0.1
+# Last Updated Date: 10-Sept-2019
+# Version 2.0.0
 #
 # Description:
 #  Helpful utility to
@@ -23,7 +23,8 @@
 
 COLOR_WHITE='\033[0m'
 COLOR_GREEN='\e[0;32m'
-FILE="/opt/tmp/dhcp_staticlist.txt"
+DHCP_STATICLIST="/opt/tmp/dhcp_staticlist.txt"
+DHCP_HOSTNAMES="/opt/tmp/dhcp_hostnames.txt"
 
 Menu_DHCP_Staticlist() {
 
@@ -37,7 +38,7 @@ Menu_DHCP_Staticlist() {
     printf '%b[4]%b - Append DHCP Static List to dnsmasq.conf.add & Disable DHCP Manual Assignment\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf '%b[5]%b - Disable DHCP Manual Assignment\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf '%b[6]%b - Enable DHCP Manual Assignment\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-    printf '%b[7]%b - Save nvram dhcp_staticlist to /opt/tmp/dhcp_staticlist.txt and clear dhcp_staticlist\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+    printf '%b[7]%b - Backup nvram dhcp_staticlist to /opt/tmp/dhcp_staticlist.txt and dhcp_hostnames to /opt/tmp/dhcp_hostnames.txt and clear nvram values\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf '%b[8]%b - Display character size of dhcp_staticlist (2999 is the limit)\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf '%b[e]%b - Exit\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     echo
@@ -47,6 +48,7 @@ Menu_DHCP_Staticlist() {
     case "$option" in
     1)
       Save_DHCP_Staticlist
+      Save_DHCP_Hostnames
       echo
       echo "Press enter to continue"
       read -r
@@ -55,6 +57,7 @@ Menu_DHCP_Staticlist() {
       ;;
     2)
       Restore_DHCP_Staticlist
+      Restore_DHCP_Hostnames
       echo
       echo "Press enter to continue"
       read -r
@@ -121,6 +124,7 @@ Menu_DHCP_Staticlist() {
       ;;
     7)
       Save_DHCP_Staticlist
+      Save_DHCP_Hostnames
       nvram unset dhcp_staticlist
       nvram set dhcp_static_x=0
       nvram commit
@@ -161,31 +165,39 @@ Make_Backup() {
 
   FILE="$1"
   TIMESTAMP="$(date +%Y-%m-%d_%H-%M-%S)"
-  BACKUP_FILE_NAME="${FILE}.${TIMESTAMP}"
+  BACKUP_FILE="${FILE}.${TIMESTAMP}"
 
   if [ -s "$FILE" ]; then
-    if ! mv "$FILE" "$BACKUP_FILE_NAME" >/dev/null 2>&1; then
-      printf 'Error backing up existing %b%s%b to %b%s%b\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE" "$COLOR_GREEN" "$BACKUP_FILE_NAME" "$COLOR_WHITE"
+    if ! mv "$FILE" "$BACKUP_FILE" >/dev/null 2>&1; then
+      printf 'Error backing up existing %b%s%b to %b%s%b\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE" "$COLOR_GREEN" "$BACKUP_FILE" "$COLOR_WHITE"
       printf 'Exiting %s\n' "$(basename "$0")"
       exit 1
     else
       printf 'Existing %b%s%b found\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE"
-      printf '%b%s%b backed up to %b%s%b\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE" "$COLOR_GREEN" "$BACKUP_FILE_NAME" "$COLOR_WHITE"
+      printf '%b%s%b backed up to %b%s%b\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE" "$COLOR_GREEN" "$BACKUP_FILE" "$COLOR_WHITE"
     fi
   fi
 }
 
 Save_DHCP_Staticlist() {
-  if [ -s "$FILE" ]; then
-    Make_Backup "$FILE"
+  if [ -s "$DHCP_STATICLIST" ]; then
+    Make_Backup "$DHCP_STATICLIST"
   fi
 
-  nvram get dhcp_staticlist >"$FILE" && echo "dhcp_staticlist nvram values successfully stored in $FILE" || echo "Unknown error occurred trying to save $FILE"
+  nvram get dhcp_staticlist >"$DHCP_STATICLIST" && echo "dhcp_staticlist nvram values successfully stored in $DHCP_STATICLIST" || echo "Unknown error occurred trying to save $DHCP_STATICLIST"
+}
+
+Save_DHCP_Hostnames() {
+  if [ -s "$DHCP_HOSTNAMES" ]; then
+    Make_Backup "$DHCP_HOSTNAMES"
+  fi
+
+  nvram get dhcp_hostnames >"$DHCP_HOSTNAMES" && echo "dhcp_hostnames nvram values successfully stored in $DHCP_HOSTNAMES" || echo "Unknown error occurred trying to save $DHCP_HOSTNAMES"
 }
 
 Restore_DHCP_Staticlist() {
 
-  nvram set dhcp_staticlist="$(cat "$FILE")"
+  nvram set dhcp_staticlist="$(cat "$DHCP_STATICLIST")"
   nvram commit
   if [ -n "$(nvram get dhcp_staticlist >/dev/null 2>&1)" ]; then
     echo "dhcp_staticlist successfully restored"
@@ -194,10 +206,84 @@ Restore_DHCP_Staticlist() {
   fi
 }
 
+Restore_DHCP_Hostnames() {
+
+  nvram set dhcp_hostnames="$(cat "$DHCP_HOSTNAMES")"
+  nvram commit
+  if [ -n "$(nvram get dhcp_hostnames >/dev/null 2>&1)" ]; then
+    echo "dhcp_hostnames successfully restored"
+  else
+    echo "Unknown error occurred trying to restore dhcp_hostnames"
+  fi
+}
+
 Save_Dnsmasq_Format() {
 
-  nvram get dhcp_staticlist | sed 's/</\n/g' | grep ":" | awk -F">" '{ print "<"$2">"$1">"$3; }' | \
-  sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n | awk -F">" '{ print "dhcp-host="$2","$1","$3; }' | sed s/\<//
+  # Retrieve Static DHCP assignments MAC and IP Address; remove < and > symbols and separate fields with a space.
+  nvram get dhcp_staticlist | sed 's/<//;s/>/ /g;s/</ /g' >/tmp/staticlist.$$
+
+  # Retrieve Static DHCP assignments MAC and hostname; remove < and > symbols and separate fields with a space.
+
+  if [ -s /jffs/nvram/dhcp_hostnames ]; then #HND Routers store hostnames in a file
+    awk '{print $0}' /jffs/nvram/dhcp_hostnames | sed 's/<//;s/>/ /g;s/</ /g' >/tmp/hostnames.$$
+  else
+    nvram get dhcp_hostnames | sed 's/<//;s/>/ /g;s/</ /g' >/tmp/hostnames.$$
+  fi
+  # count number of fields in the file
+  word_count_staticlist=$(head -1 /tmp/staticlist.$$ | wc -w)
+  word_count_hostnames=$(head -1 /tmp/hostnames.$$ | wc -w)
+
+  if [ "$word_count_staticlist" -ne "$word_count_hostnames" ]; then
+    echo "Unexpected error condition dhcp_staticlist and dhcp_hostnames don't match"
+  else
+    # count number of static leases. This is the number of loops required to get IP address and client name
+    # divide word_count by 2 since client information is listed in groups of 2 fields: MAC_Address and IP_Address
+    static_leases_count=$((word_count_staticlist / 2))
+  fi
+
+  # write MAC and IP Addresses for Static DHCP LAN Clients to /tmp/MACIP.$$
+  true >/tmp/MACIP.$$
+
+  loop_count=1
+  MAC=1
+  IP=2
+
+  while [ "$loop_count" -le "$static_leases_count" ]; do
+    cut -d' ' -f"$MAC","$IP" </tmp/staticlist.$$ >>"/tmp/MACIP.$$"
+    MAC=$((MAC + 2))
+    IP=$((IP + 2))
+    loop_count=$((loop_count + 1))
+  done
+
+  # write MAC and HOSTNAME for Static DHCP LAN Clients to /tmp/MACHOSTNAMES.$$
+  true >/tmp/MACHOSTNAMES.$$
+
+  loop_count=1
+  MAC=1
+  HOSTNAME=2
+
+  while [ "$loop_count" -le "$static_leases_count" ]; do
+    cut -d' ' -f"$MAC","$HOSTNAME" </tmp/hostnames.$$ >>"/tmp/MACHOSTNAMES.$$"
+    MAC=$((MAC + 2))
+    HOSTNAME=$((HOSTNAME + 2))
+    loop_count=$((loop_count + 1))
+  done
+
+  # Join the two files together to form one file containing MAC, IP, HOSTNAME
+  awk '
+    NR==FNR { k[$1]=$2; next }
+    { print $0, k[$1] }
+  ' /tmp/MACIP.$$ /tmp/MACHOSTNAMES.$$ >/tmp/MACIPHOSTNAMES.$$
+
+  # write dhcp-host entry to /jffs/configs/dnsmasq.conf.add
+  #
+  sort -t . -k 3,3n -k 4,4n /tmp/MACIPHOSTNAMES.$$ | awk '{ print "dhcp-host="$1","$2","$3; }'
+
+  rm -rf /tmp/staticlist.$$
+  rm -rf /tmp/hostnames.$$
+  rm -rf /tmp/MACIP.$$
+  rm -rf /tmp/MACHOSTNAMES.$$
+  rm -rf /tmp/MACIPHOSTNAMES.$$
 }
 
 clear
