33a1ce6a383ecc7cef908d1c4cfdf5d4942c4afa
Xentrk <xentrk@x3mtek.com>
2019-09-18 19:37:24 +0700

    account for the undefined in nvram list

--- dhcpstaticlist.sh
+++ dhcpstaticlist.sh
@@ -2,9 +2,9 @@
 ####################################################################################################
 # Script: dhcpstaticlist.sh
 # Original Author: Xentrk
-# Last Updated Date: 17-Sept-2019
+# Last Updated Date: 18-Sept-2019
 # Compatible with 384.13
-# Version 2.0.1
+# Version 2.0.2
 #
 # Description:
 #  Helpful utility to
@@ -274,14 +274,14 @@ Save_Dnsmasq_Format() {
 
   # Retrieve Static DHCP assignments MAC and IP Address; remove < and > symbols and separate fields with a space.
   if [ -s /jffs/nvram/dhcp_staticlist ]; then #HND Routers store dhcp_staticlist in a file
-    awk '{print $0}' /jffs/nvram/dhcp_staticlist | sed 's/<//;s/>/ /g;s/</ /g' >/tmp/staticlist.$$
+    awk '{print $0}' /jffs/nvram/dhcp_staticlist | sed 's/<//;s/>undefined//;s/>/ /g;s/</ /g' >/tmp/staticlist.$$
   else
     nvram get dhcp_staticlist | sed 's/<//;s/>/ /g;s/</ /g' >/tmp/staticlist.$$
   fi
 
   # Retrieve Static DHCP assignments MAC and hostname; remove < and > symbols and separate fields with a space.
   if [ -s /jffs/nvram/dhcp_hostnames ]; then #HND Routers store hostnames in a file
-    awk '{print $0}' /jffs/nvram/dhcp_hostnames | sed 's/<//;s/>/ /g;s/</ /g' >/tmp/hostnames.$$
+    awk '{print $0}' /jffs/nvram/dhcp_hostnames | sed 's/<//;s/>undefined//;s/>/ /g;s/</ /g' >/tmp/hostnames.$$
   else
     nvram get dhcp_hostnames | sed 's/<//;s/>/ /g;s/</ /g' >/tmp/hostnames.$$
   fi
